7668579f8c1f2d4b1a03419261802f19547fbad9
Muhammad-Taimur <muhammadtaimurhassan@gmail.com>
2021-06-27 00:22:37 +0100

    aaa

--- EStore.Service/Startup.cs
+++ EStore.Service/Startup.cs
@@ -12,6 +12,7 @@ using Serilog;
 //TO Start Docker container use the following command in PowerShell
 //docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 
+    
 namespace EStore.Service
 {
     public class Startup
